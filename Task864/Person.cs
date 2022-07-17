929378bebc228408861838241ade12e0e7df088e
Seva <Seva@DESKTOP-CLQ02HK>
2022-07-17 11:50:38 +0300

    Добавьте файлы проекта.

---
+++ Task864/Person.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+
+namespace Task864
+{
+    public struct Person
+    {
+        #region Поля
+
+        public string name { get; set; }
+        public string street { get; set; }
+        public string houseNumber { get; set; }
+        public string flatNumber { get; set; }
+        public string mobilePhone { get; set; }
+        public string flatPhone { get; set; }
+
+        #endregion
+
+        #region Конструктор
+        public Person(string pName, string str, string hn, string fn, string mp, string fp)
+        {
+            this.name = pName;
+            this.street = str;
+            this.houseNumber = hn;
+            this.flatNumber = fn;
+            this.mobilePhone = mp;
+            this.flatPhone = fp;
+        }
+        #endregion
+
+        #region Метод
+
+        /// <summary>
+        /// Метод, возвращающий Person в виде структурированного XElement.
+        /// </summary>
+        /// <returns></returns>
+        public XElement XmlPrint()
+        {
+
+            XElement nPerson = new XElement("Person", $"name='{name}'",
+                new XElement("Address",
+                    new XElement("Street", street),
+                    new XElement("HouseNumber", houseNumber),
+                    new XElement("FlatNumber", flatNumber)
+                    ),
+                new XElement("Phones",
+                    new XElement("MobilePhone", mobilePhone),
+                    new XElement("FlatPhone", flatPhone)
+                    )
+                );
+
+            return nPerson;
+        }
+
+        #endregion
+    }
+}
---
+++ Task864/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+
+namespace Task864
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            string path = "Контакты.xml";
+
+            Console.WriteLine("Создание контакта. \nВведите: ");
+
+
+            Console.Write("1. Ф.И.О. контакта: ");
+            string name = Console.ReadLine();
+
+            Console.Write("\n2. Улицу проживания: ");
+            string street = Console.ReadLine();
+
+            Console.Write("\n3. Номер дома: ");
+            string houseNumber = Console.ReadLine();
+
+            Console.Write("\n4. Номер квартиры: ");
+            string flatNumber = Console.ReadLine();
+
+            Console.Write("\n5. Мобильный телефон контакта: ");
+            string mobilePhone = Console.ReadLine();
+
+            Console.Write("\n6. Домашний телефон контакта: ");
+            string flatPhone = Console.ReadLine();
+
+            Person p = new Person(name, street, houseNumber, flatNumber, mobilePhone, flatPhone);
+
+            Console.WriteLine(p.XmlPrint());
+            Save(path, p);
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Метод, сохраняющий Person в .xml файл.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="p"></param>
+        static void Save(string path, Person p)
+        {
+            StreamWriter sw = new StreamWriter(path, false);
+
+            sw.Write(p.XmlPrint());
+
+            sw.Close();
+        }
+    }
+}
